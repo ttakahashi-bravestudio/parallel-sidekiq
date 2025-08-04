cc29335dc3417ef2ab082cb42f1b9e51a4228a4c
ttakahashi-bravestudio <t.takahashi@bravestudio.co.jp>
2025-08-04 19:27:26 +0900

    CSV処理ジョブにおいて、完全なランダム値を含むtxtファイルを作成する機能を追加しました。また、以前のxlsxおよびpdf生成処理をコメントアウトしました。

--- app/sidekiq/report/process_csv_row_job.rb
+++ app/sidekiq/report/process_csv_row_job.rb
@@ -4,9 +4,16 @@ class Report::ProcessCsvRowJob
 
   def perform(row, type, report_id, path, token)
     FileUtils.mkdir_p(path)
-    svc = ReportService.new(row['個別識別番号'], row['保険者名'], row['年度'])
-    svc.generate_xlsx(path, row['個別識別番号'])
-    svc.generate_pdf(path, row['個別識別番号']) if type == 'pdf'
+    # svc = ReportService.new(row['個別識別番号'], row['保険者名'], row['年度'])
+    # svc.generate_xlsx(path, row['個別識別番号'])
+    # svc.generate_pdf(path, row['個別識別番号']) if type == 'pdf'
+
+    # 完全なランダム値でtxtファイルを作成する処理（ファイル名にtokenを含める）
+    filename = "random_#{token}_#{SecureRandom.hex(8)}.txt"
+    random_content = Array.new(5) { SecureRandom.hex(16) }.join("\n")
+    File.open(File.join(path, filename), "w") do |f|
+      f.puts random_content
+    end
 
     CsvProcessingStatus.where(token:).update_all("done_count = done_count + 1")
   end
